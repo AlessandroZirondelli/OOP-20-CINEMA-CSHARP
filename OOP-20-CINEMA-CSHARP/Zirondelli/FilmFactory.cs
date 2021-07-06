1ceed99ee7bdf6defa7f1ae6ba7ea0abbf6f7c49
AlessandroZirondelli <alessandr.zirondelli@studio.unibo.it>
2021-07-06 19:26:34 +0200

    Add factory

---
+++ OOP-20-CINEMA-CSHARP/Zirondelli/FilmFactory.cs
@@ -0,0 +1,26 @@
+﻿using OOP20_CINEMA_CSHARP.Zirondelli;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_20_CINEMA_CSHARP.Zirondelli
+{
+    public class FilmFactory : IFilmFactory
+    {
+
+        private ManagerIdsFilms managerIds; /** Manager for film ids. It's used to assign a new id for a new film. */
+        public FilmFactory( ManagerIdsFilms managerIds)
+        {
+            this.managerIds = managerIds;
+        }
+        public IFilm createBasicFilm(string name, string genre, string description, string coverPath, int duration)
+        {
+            return new Film(managerIds.GetNewFilmID(), name, genre, duration, description, coverPath);
+        }
+
+        public IFilm createBasicFilmById(string name, string genre, string description, string coverPath, int duration, int id)
+        {
+            return new Film(id, name, genre, duration, description, coverPath);
+        }
+    }
+}
---
+++ OOP-20-CINEMA-CSHARP/Zirondelli/IFilmFactory.cs
@@ -0,0 +1,13 @@
+﻿using OOP20_CINEMA_CSHARP.Zirondelli;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_20_CINEMA_CSHARP.Zirondelli
+{
+    interface IFilmFactory
+    {
+        IFilm createBasicFilm(String name, String genre, String description, string ? coverPath, int duration);
+        IFilm createBasicFilmById(String name, String genre, String description, string? coverPath, int duration, int id);
+    }
+}
