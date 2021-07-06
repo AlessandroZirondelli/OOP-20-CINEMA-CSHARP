e7aee7e2817a4578f6f8ab51b025e730e13ce6e6
AlessandroZirondelli <alessandr.zirondelli@studio.unibo.it>
2021-07-06 19:32:58 +0200

    Add factoory 2

---
+++ OOP-20-CINEMA-CSHARP/Zirondelli/IProgrammedFilmFactory.cs
@@ -0,0 +1,12 @@
+﻿using OOP20_CINEMA_CSHARP.Zirondelli;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_20_CINEMA_CSHARP.Zirondelli
+{
+    interface IProgrammedFilmFactory
+    {
+        IProgrammedFilm createProgrammedFilm(int idFilm, Hall hall, double price, DateTime date, DateTime startTime, DateTime endTime);
+    }
+}
---
+++ OOP-20-CINEMA-CSHARP/Zirondelli/ProgrammedFilmFactory.cs
@@ -0,0 +1,15 @@
+﻿using OOP20_CINEMA_CSHARP.Zirondelli;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_20_CINEMA_CSHARP.Zirondelli
+{
+    class ProgrammedFilmFactory : IProgrammedFilmFactory
+    {
+        public IProgrammedFilm createProgrammedFilm(int idFilm, Hall hall, double price, DateTime date, DateTime startTime, DateTime endTime)
+        {
+            return new ProgrammedFilm(idFilm, hall, price, date, new TimeSlot(startTime, endTime));
+        }
+    }
+}
